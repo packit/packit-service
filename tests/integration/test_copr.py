f003cf17e8e674c680f97d0aba51827e80a4b163
Radoslav Pitonak <rado.pitonak@gmail.com>
2019-10-14 14:30:21 +0200

    First shot on integration tests for copr build

--- tests/integration/test_copr.py
+++ tests/integration/test_copr.py
@@ -1,81 +1,76 @@
+# MIT License
+#
+# Copyright (c) 2018-2019 Red Hat, Inc.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import json
-from pathlib import Path
-
 import pytest
-from copr.v3.client import Client as CoprClient
-from flexmock import flexmock
-from github import Github
-from ogr.services.github import GithubProject
-from packit.api import PackitAPI
-from packit.local_project import LocalProject
 
-from packit_service.service.models import Model
-from packit_service.worker import jobs
-from packit_service.worker.handler import BuildStatusReporter
 from packit_service.worker.jobs import SteveJobs
-from packit_service.worker.whitelist import Whitelist
 from tests.spellbook import DATA_DIR
 
 
 @pytest.fixture()
 def pr_event():
-    return json.loads((DATA_DIR / "webhooks" / "github_pr_event.json").read_text())
+    return json.loads(
+        (DATA_DIR / "webhooks" / "copr_build" / "pr_synchronize.json").read_text()
+    )
 
 
 @pytest.fixture()
-def release_event():
-    return json.loads((DATA_DIR / "webhooks" / "release_event.json").read_text())
-
-
-@pytest.mark.xfail  # Update once ogr mocking problems are resolved
-def test_copr_pr_handle(pr_event, dump_http_com):
-    config = dump_http_com(f"{Path(__file__).name}/pr_handle.yaml")
-    flexmock(jobs.SteveJobs, config=config)
-
-    # it would make sense to make LocalProject offline
-    flexmock(PackitAPI).should_receive("run_copr_build").with_args(
-        owner="packit",
-        project="packit-service-packit-342",
-        chroots=["fedora-29-x86_64", "fedora-rawhide-x86_64", "fedora-30-x86_64"],
-    ).and_return("1", "asd").once()
-    flexmock(PackitAPI).should_receive("watch_copr_build").and_return("failed").once()
-
-    jobs.SteveJobs().process_message(pr_event)
-
-
-def test_wrong_collaborator(pr_event):
-    packit_yaml = (
-        "{'specfile_path': '', 'synced_files': []"
-        ", jobs: [{trigger: pull_request, job: copr_build, metadata: {targets:[]}}]}"
+def pr_comment_event():
+    return json.loads(
+        (DATA_DIR / "webhooks" / "copr_build" / "pr_comment.json").read_text()
     )
-    copr_dict = {
-        "login": "stevejobs",
-        "username": "stevejobs",
-        "token": "apple",
-        "copr_url": "https://copr.fedorainfracloud.org",
-    }
-    flexmock(Github, get_repo=lambda full_name_or_id: None)
-    flexmock(
-        GithubProject,
-        get_file_content=lambda path, ref: packit_yaml,
-        full_repo_name="packit-service/hello-world",
-    )
-    flexmock(GithubProject).should_receive("who_can_merge_pr").and_return({"foobar"})
-    flexmock(BuildStatusReporter).should_receive("set_status").and_return(None)
-    flexmock(
-        LocalProject,
-        refresh_the_arguments=lambda: None,
-        checkout_pr=lambda full_name_or_id: None,
-    )
-    flexmock(CoprClient).should_receive("create_from_config_file").and_return(
-        CoprClient(copr_dict)
-    )
-    flexmock(Whitelist, check_and_report=True)
-    flexmock(Model, save=lambda: None)
 
+
+@pytest.fixture()
+def pr_comment_event_not_collaborator():
+    return json.loads(
+        (
+            DATA_DIR / "webhooks" / "copr_build" / "pr_comment_not_collaborator.json"
+        ).read_text()
+    )
+
+
+def test_submit_copr_build_pr_event(pr_event):
     steve = SteveJobs()
-    flexmock(SteveJobs, _is_private=False)
     result = steve.process_message(pr_event)
+
+    assert result
+    assert "copr_build" in result["jobs"]
+    assert result["jobs"]["copr_build"] == "success"
+
+
+def test_submit_copr_build_pr(pr_comment_event):
+    steve = SteveJobs()
+    result = steve.process_message(pr_comment_event)
+
+    assert result
+    assert "copr_build" in result["jobs"]
+    assert result["jobs"]["copr_build"] == "success"
+
+
+def test_not_collaborator(pr_comment_event_not_collaborator):
+    steve = SteveJobs()
+    result = steve.process_message(pr_comment_event_not_collaborator)
     copr_build = result["jobs"]["copr_build"]
     assert not copr_build["success"]
     assert (
