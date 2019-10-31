ea3b49334a21e888419261852ba72df1d602afb1
Radoslav Pitonak <rado.pitonak@gmail.com>
2019-10-31 13:40:51 +0100

    requre take responses when they are generated

--- tests_requre/openshift_integration/base.py
+++ tests_requre/openshift_integration/base.py
@@ -6,25 +6,33 @@ from requre.utils import STORAGE
 from packit.config import RunCommandType
 from packit_service.worker.jobs import SteveJobs
 
+TEST_DATA_DIR = "test_data"
 PROJECT_DIR = Path(__file__).parent.parent.parent
-PERSISTENT_DATA_PREFIX = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "test_data"
-)
+PERSISTENT_DATA_PREFIX = Path(__file__).parent.parent / TEST_DATA_DIR
 DATA_DIR = PROJECT_DIR / "tests" / "data"
 
 
 class PackitServiceTestCase(unittest.TestCase):
-    def get_datafile_filename(self, path_prefix=PERSISTENT_DATA_PREFIX, suffix="yaml"):
+    def get_datafile_filename(
+        self, path_prefix: Path = PERSISTENT_DATA_PREFIX, suffix="yaml"
+    ) -> Path:
         test_file_name = os.path.basename(inspect.getfile(self.__class__)).rsplit(
             ".", 1
         )[0]
         test_class_name = f"{self.id()}.{suffix}"
-        testdata_dirname = os.path.join(path_prefix, test_file_name)
-        os.makedirs(testdata_dirname, mode=0o777, exist_ok=True)
-        return os.path.join(testdata_dirname, test_class_name)
+        testdata_dirname = path_prefix / str(test_file_name)
+        testdata_dirname.mkdir(mode=0o777, exist_ok=True)
+        return testdata_dirname / test_class_name
 
     def setUp(self) -> None:
-        STORAGE.storage_file = self.get_datafile_filename(path_prefix="/tmp/test_data")
+        if self.get_datafile_filename().exists():
+            # if already exists, do not regenerate test file, what is stored inside tests dir
+            STORAGE.storage_file = str(self.get_datafile_filename())
+        else:
+            # store them to path where Persistent volume is mounted
+            STORAGE.storage_file = str(
+                self.get_datafile_filename(path_prefix=Path("/tmp") / TEST_DATA_DIR)
+            )
         self.steve = SteveJobs()
         self.steve.config.command_handler = RunCommandType.local
         self.steve.config.command_handler_work_dir = "/tmp/hello-world"
