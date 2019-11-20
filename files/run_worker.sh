541dd8ab6f90c022c5e56542a7b5f2bd943c7521
Jiri Popelka <jpopelka@redhat.com>
2019-11-20 17:47:32 +0100

    [run_worker.sh] loglevel=debug also @ stg

--- files/run_worker.sh
+++ files/run_worker.sh
@@ -6,20 +6,20 @@ if [[ -z ${APP} ]]; then
     exit 1
 fi
 
-if [[ ${DEPLOYMENT} == "dev" ]]; then
-    LOGLEVEL="debug"
+if [[ ${DEPLOYMENT} == "prod" ]]; then
+  LOGLEVEL="info"
 else
-    LOGLEVEL="info"
+  LOGLEVEL="debug"
 fi
 
 source /src-packit-service/files/setup_env_in_openshift.sh
 
-mkdir --mode=0700 -p ${PACKIT_HOME}/.ssh
-pushd ${PACKIT_HOME}/.ssh
+mkdir --mode=0700 -p "${PACKIT_HOME}/.ssh"
+pushd "${PACKIT_HOME}/.ssh"
 install -m 0400 /packit-ssh/id_rsa .
 install -m 0400 /packit-ssh/id_rsa.pub .
 install -m 0400 /packit-ssh/config .
 grep -q pkgs.fedoraproject.org known_hosts || ssh-keyscan pkgs.fedoraproject.org >>known_hosts
 popd
 
-exec celery-3 worker --app=${APP} --loglevel=${LOGLEVEL} --concurrency=1
+exec celery-3 worker --app="${APP}" --loglevel=${LOGLEVEL} --concurrency=1
