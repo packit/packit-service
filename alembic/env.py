f90dd6b158863614dd303089705af7e66bf2da8d
Anchit <ab@abifog.com>
2020-03-04 23:52:59 +0530

    Add tests and fix mypy

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,9 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from packit_service import models
+from typing import TYPE_CHECKING
+
+from packit_service.models import Base
 from packit_service.models import get_pg_url
 
 
@@ -20,7 +22,12 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = models.Base.metadata
+
+# issue highlighted in sqlalchemy models.py
+if TYPE_CHECKING:
+    pass
+else:
+    target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
