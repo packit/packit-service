696f44e65dd8ba7dd931f880d4ec4b062e22aa36
Anchit Bajaj <ab@abifog.com>
2020-03-05 14:54:54 +0530

    Update alembic/env.py
    Co-Authored-By: Tomas Tomecek <tomas@tomecek.net>

--- alembic/env.py
+++ alembic/env.py
@@ -24,9 +24,7 @@ fileConfig(config.config_file_name)
 # target_metadata = mymodel.Base.metadata
 
 # issue highlighted in sqlalchemy models.py
-if TYPE_CHECKING:
-    pass
-else:
+if not TYPE_CHECKING:
     target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
