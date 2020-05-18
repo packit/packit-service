464761349676dcb00344bcce34bc4cec75220ec4
Frantisek Lachman <flachman@redhat.com>
2020-05-18 15:11:24 +0200

    Implement koji build
    Exclude SRPM creation from build and use directly SRPMBuildModel.
    - We can share code between koji and copr build. The workflow is now easier to follow.
    - The metadata class for build was removed and we use directly the database object instead.
    
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- packit_service/worker/build/koji_build.py
+++ packit_service/worker/build/koji_build.py
@@ -20,10 +20,26 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import logging
+from re import search
+from typing import Optional, Union, Tuple
 
-from packit.config import JobType
-
-from packit_service.worker.build.copr_build import BaseBuildJobHelper
+from ogr.abstract import CommitStatus, GitProject
+from packit.config import JobType, PackageConfig, JobConfig
+from packit_service import sentry_integration
+from packit_service.config import ServiceConfig
+from packit_service.constants import MSG_RETRIGGER
+from packit_service.models import KojiBuildModel
+from packit_service.service.events import (
+    PullRequestGithubEvent,
+    PullRequestCommentGithubEvent,
+    PushGitHubEvent,
+    ReleaseEvent,
+)
+from packit_service.service.urls import (
+    get_srpm_log_url_from_flask,
+    get_koji_build_log_url_from_flask,
+)
+from packit_service.worker.build.build_helper import BaseBuildJobHelper
 from packit_service.worker.result import HandlerResults
 
 logger = logging.getLogger(__name__)
@@ -32,6 +48,116 @@ logger = logging.getLogger(__name__)
 class KojiBuildJobHelper(BaseBuildJobHelper):
     job_type_build = JobType.production_build
     job_type_test = None
+    status_name_build: str = f"production-build"
+    status_name_test: str = None
 
-    def run_koji_build(self, scratch: bool = False) -> HandlerResults:
-        raise NotImplementedError()
+    def __init__(
+        self,
+        config: ServiceConfig,
+        package_config: PackageConfig,
+        project: GitProject,
+        event: Union[
+            PullRequestGithubEvent,
+            PullRequestCommentGithubEvent,
+            PushGitHubEvent,
+            ReleaseEvent,
+        ],
+        job: Optional[JobConfig] = None,
+    ):
+        super().__init__(config, package_config, project, event, job)
+        self.msg_retrigger: str = MSG_RETRIGGER.format(build="production-build")
+
+    @property
+    def is_scratch(self) -> bool:
+        return self.job_build and self.job_build.metadata.scratch
+
+    def run_koji_build(self) -> HandlerResults:
+        self.report_status_to_all(
+            description="Building SRPM ...", state=CommitStatus.pending
+        )
+        self.create_srpm_if_needed()
+
+        if not self.srpm_model.success:
+            msg = "SRPM build failed, check the logs for details."
+            self.report_status_to_all(
+                state=CommitStatus.failure,
+                description=msg,
+                url=get_srpm_log_url_from_flask(self.srpm_model.id),
+            )
+            return HandlerResults(success=False, details={"msg": msg})
+
+        for chroot in self.build_chroots:
+
+            try:
+                build_id, web_url = self.run_build()
+            except Exception as ex:
+                sentry_integration.send_to_sentry(ex)
+                # TODO: Where can we show more info about failure?
+                # TODO: Retry
+                self.report_status_to_all(
+                    state=CommitStatus.error,
+                    description=f"Submit of the build failed: {ex}",
+                    url=get_koji_build_log_url_from_flask(self.srpm_model.id),
+                )
+                return HandlerResults(success=False, details={"error": str(ex)})
+
+            koji_build = KojiBuildModel.get_or_create(
+                build_id=str(build_id),
+                commit_sha=self.event.commit_sha,
+                web_url=web_url,
+                target=chroot,
+                status="pending",
+                srpm_build=self.srpm_model,
+                trigger_model=self.event.db_trigger,
+            )
+            url = get_koji_build_log_url_from_flask(id_=koji_build.id)
+            self.report_status_to_all_for_chroot(
+                state=CommitStatus.pending,
+                description="Building RPM ...",
+                url=url,
+                chroot=chroot,
+            )
+
+        # TODO: release the hounds!
+        """
+        celery_app.send_task(
+            "task.babysit_koji_build",
+            args=(build_metadata.build_id,),
+            countdown=120,  # do the first check in 120s
+        )
+        """
+
+        return HandlerResults(success=True, details={})
+
+    def run_build(
+        self, target: Optional[str] = None
+    ) -> Tuple[Optional[int], Optional[str]]:
+        if not target:
+            logger.debug("No targets set for koji build, using rawhide.")
+            target = "rawhide"
+
+        out = self.api.up.koji_build(
+            scratch=self.is_scratch,
+            nowait=True,
+            koji_target=target,
+            srpm_path=self.srpm_path,
+        )
+        if not out:
+            return None, None
+
+        # packit does not return any info about build.
+        # TODO: move the parsing to packit
+        task_id, task_url = None, None
+
+        task_id_match = search(pattern=r"Created task: (\d+)", string=out)
+        if task_id_match:
+            task_id = int(task_id_match.group(1))
+
+        task_url_match = search(
+            pattern=r"(https://koji\.fedoraproject\.org/koji/taskinfo\?taskID=\d+)",
+            string=out,
+        )
+        if task_url_match:
+            task_url = task_url_match.group(0)
+
+        return task_id, task_url
