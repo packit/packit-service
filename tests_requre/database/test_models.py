b5e3914e09728eed4a1441c279ea55a3516724bb
IceWreck <ab@abifog.com>
2020-08-13 08:44:14 +0000

    Test /api/srpm-builds endpoint

--- tests_requre/database/test_models.py
+++ tests_requre/database/test_models.py
@@ -275,7 +275,14 @@ def test_errors_while_doing_db(clean_before_and_after):
         assert len(session.query(PullRequestModel).all()) == 1
 
 
-# return all builds in table
+# return srpm builds in given range
+def test_get_srpm_builds(clean_before_and_after, srpm_build_model):
+    builds_list = SRPMBuildModel.get(0, 10)
+    assert len(builds_list) == 1
+    assert builds_list[0].success is True
+
+
+# return all copr builds in table
 def test_get_all_builds(clean_before_and_after, multiple_copr_builds):
     builds_list = list(CoprBuildModel.get_all())
     assert len(builds_list) == 3
@@ -284,7 +291,7 @@ def test_get_all_builds(clean_before_and_after, multiple_copr_builds):
     assert builds_list[1].status == "pending"
 
 
-# return all builds with given build_id
+# return all copr builds with given build_id
 def test_get_all_build_id(clean_before_and_after, multiple_copr_builds):
     builds_list = list(CoprBuildModel.get_all_by_build_id(str(123456)))
     assert len(builds_list) == 2
@@ -293,7 +300,7 @@ def test_get_all_build_id(clean_before_and_after, multiple_copr_builds):
     assert builds_list[1].project_name == "the-project-name"
 
 
-# returns the first build with given build id and target
+# returns the first copr build with given build id and target
 def test_get_by_build_id(clean_before_and_after, multiple_copr_builds):
     # these are not iterable and thus should be accessible directly
     build_a = CoprBuildModel.get_by_build_id(SampleValues.build_id, SampleValues.target)
@@ -603,6 +610,13 @@ def test_pr_get_copr_builds(
     pr_model = a_copr_build_for_pr.job_trigger.get_trigger_object()
     assert a_copr_build_for_pr in pr_model.get_copr_builds()
     assert not different_pr_model.get_copr_builds()
+
+
+def test_pr_get_srpm_builds(
+    clean_before_and_after, a_copr_build_for_pr, srpm_build_model
+):
+    pr_model = a_copr_build_for_pr.job_trigger.get_trigger_object()
+    assert srpm_build_model in pr_model.get_srpm_builds()
 
 
 def test_bugzilla_create(clean_before_and_after, bugzilla_model):
