05220a4dd3570d91e182b264d2790056474f164e
Jan Ščotka <jscotka@redhat.com>
2020-10-12 12:27:24 +0200

    new version of decorators in requre

--- tests_requre/openshift_integration/base.py
+++ tests_requre/openshift_integration/base.py
@@ -1,7 +1,7 @@
 import os
 from shutil import copy
 from pathlib import Path
-from requre import RequreTestCase
+import unittest
 from requre.constants import RELATIVE_TEST_DATA_DIRECTORY
 from requre.cassette import StorageMode
 from packit.config import RunCommandType
@@ -12,23 +12,28 @@ PROJECT_DIR = Path(__file__).parent.parent.parent
 DATA_DIR = PROJECT_DIR / "tests" / "data"
 
 
-class PackitServiceTestCase(RequreTestCase):
-    def setUp(self) -> None:
-        super().setUp()
-        self.steve = SteveJobs()
-        self.steve.service_config.command_handler = RunCommandType.local
-        self.steve.service_config.command_handler_work_dir = "/tmp/hello-world"
+class PackitServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        self._steve = None
 
-    def tearDown(self):
-        super().tearDown()
+    @property
+    def steve(self):
+        if not self._steve:
+            self._steve = SteveJobs()
+            self._steve.service_config.command_handler = RunCommandType.local
+            self._steve.service_config.command_handler_work_dir = "/tmp/hello-world"
+        return self._steve
+
+    def cassette_teardown(self, cassette):
         # copy files to destination, where is mounted persistent volume
-        if self.cassette.mode == StorageMode.write:
+        cassette.dump()
+        if cassette.mode == StorageMode.write:
             destdir = (
                 Path("/tmp")
                 / Path(RELATIVE_TEST_DATA_DIRECTORY)
-                / Path(self.cassette.storage_file).parent.name
+                / Path(cassette.storage_file).parent.name
             )
             os.makedirs(destdir, exist_ok=True)
-            storage_file = Path(self.cassette.storage_file)
+            storage_file = Path(cassette.storage_file)
             for filename in glob(f"{storage_file}*"):
                 copy(filename, destdir)
