bb15919644cb906176745ead914fb075c1ec8ef6
Jiri Popelka <jpopelka@redhat.com>
2020-11-11 19:11:50 +0100

    Make mypy happy

--- packit_service/sentry_integration.py
+++ packit_service/sentry_integration.py
@@ -1,27 +1,12 @@
-# MIT License
-#
-# Copyright (c) 2018-2019 Red Hat, Inc.
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import logging
 from contextlib import contextmanager
 from os import getenv
+from typing import List
+
+from sentry_sdk.integrations import Integration
 
 from packit_service.utils import only_once
 
@@ -49,7 +34,7 @@ def configure_sentry(
     # so that we don't have to have sentry sdk installed locally
     import sentry_sdk
 
-    integrations = []
+    integrations: List[Integration] = []
 
     if celery_integration:
         # https://docs.sentry.io/platforms/python/celery/
