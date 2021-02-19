b7711c4f5d4426a6859f7e0a0c5a45f3a6eda5e1
Matej Focko <mfocko@redhat.com>
2021-02-19 16:06:29 +0100

    Refactor propose-downstream integration tests
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- tests/integration/test_issue_comment.py
+++ tests/integration/test_issue_comment.py
@@ -26,14 +26,13 @@ from datetime import datetime
 import pytest
 from celery.canvas import Signature
 from flexmock import flexmock
-from github import Github
-from github.GitRelease import GitRelease as PyGithubRelease
 
 from ogr.abstract import GitTag
 from ogr.abstract import PullRequest, PRStatus
 from ogr.services.github import GithubProject, GithubRelease
 from ogr.services.gitlab import GitlabProject, GitlabRelease
 from packit.api import PackitAPI
+from packit.distgit import DistGit
 from packit.local_project import LocalProject
 from packit_service.config import ServiceConfig
 from packit_service.constants import SANDCASTLE_WORK_DIR
@@ -45,146 +44,81 @@ from packit_service.worker.allowlist import Allowlist
 from tests.spellbook import DATA_DIR, first_dict_value, get_parameters_from_results
 
 
-@pytest.fixture(scope="module")
-def issue_comment_propose_downstream_event():
+def issue_comment_propose_downstream_event(forge):
     return json.loads(
-        (DATA_DIR / "webhooks" / "github" / "issue_propose_downstream.json").read_text()
+        (DATA_DIR / "webhooks" / forge / "issue_propose_downstream.json").read_text()
     )
 
 
 @pytest.fixture(scope="module")
-def mock_issue_comment_functionality():
+def mock_comment(request):
+    project_class, release_class, forge, author = request.param
+
     packit_yaml = (
         "{'specfile_path': 'packit.spec', 'synced_files': [],"
         "'jobs': [{'trigger': 'release', 'job': 'propose_downstream',"
-        "'metadata': {'dist-git-branch': 'master'}}],"
+        "'metadata': {'dist-git-branch': 'main'}}],"
         "'downstream_package_name': 'packit'}"
     )
     flexmock(
-        GithubProject,
+        project_class,
         get_file_content=lambda path, ref: packit_yaml,
         full_repo_name="packit-service/packit",
+        get_web_url=lambda: f"https://{forge}.com/packit-service/packit",
+        default_branch="main",
     )
-    flexmock(Github, get_repo=lambda full_name_or_id: None)
     (
-        flexmock(GithubProject)
+        flexmock(project_class)
         .should_receive("can_merge_pr")
-        .with_args("phracek")
+        .with_args(author)
         .and_return(True)
     )
-    flexmock(GithubProject).should_receive("issue_comment").and_return(None)
-    flexmock(GithubProject).should_receive("issue_close").and_return(None)
-    gr = GithubRelease(
+    flexmock(project_class).should_receive("issue_comment").and_return(None)
+    flexmock(project_class).should_receive("issue_close").and_return(None)
+    gr = release_class(
         tag_name="0.5.1",
         url="packit-service/packit",
         created_at="",
         tarball_url="https://foo/bar",
         git_tag=flexmock(GitTag),
-        project=flexmock(GithubProject),
-        raw_release=flexmock(PyGithubRelease),
-    )
-    flexmock(GithubProject).should_receive("get_releases").and_return([gr])
-    config = ServiceConfig()
-    config.command_handler_work_dir = SANDCASTLE_WORK_DIR
-    flexmock(ServiceConfig).should_receive("get_service_config").and_return(config)
-    flexmock(LocalProject, refresh_the_arguments=lambda: None)
-    flexmock(Allowlist, check_and_report=True)
-
-
-def test_issue_comment_propose_downstream_handler(
-    mock_issue_comment_functionality, issue_comment_propose_downstream_event
-):
-    flexmock(PackitAPI).should_receive("sync_release").and_return(
-        PullRequest(
-            title="foo",
-            description="bar",
-            target_branch="baz",
-            source_branch="yet",
-            id=1,
-            status=PRStatus.open,
-            url="https://xyz",
-            author="me",
-            created=datetime.now(),
-        )
-    )
-    flexmock(
-        GithubProject,
-        get_files=lambda ref, filter_regex: [],
-        get_web_url=lambda: "https://github.com/the-namespace/the-repo",
-        is_private=lambda: False,
-    )
-
-    flexmock(IssueCommentEvent, db_trigger=IssueModel(id=123))
-    flexmock(IssueModel).should_receive("get_by_id").with_args(123).and_return(
-        flexmock(issue_id=12345)
-    )
-    flexmock(Signature).should_receive("apply_async").once()
-
-    processing_results = SteveJobs().process_message(
-        issue_comment_propose_downstream_event
-    )
-    event_dict, job, job_config, package_config = get_parameters_from_results(
-        processing_results
-    )
-
-    results = run_propose_downstream_handler(
-        package_config=package_config,
-        event=event_dict,
-        job_config=job_config,
-    )
-
-    assert first_dict_value(results["job"])["success"]
-
-
-@pytest.fixture(scope="module")
-def issue_comment_propose_downstream_gitlab_event():
-    return json.loads(
-        (DATA_DIR / "webhooks" / "gitlab" / "issue_propose_downstream.json").read_text()
-    )
-
-
-@pytest.fixture(scope="module")
-def mock_issue_comment_functionality_gitlab():
-    packit_yaml = (
-        "{'specfile_path': 'packit.spec', 'synced_files': [],"
-        "'jobs': [{'trigger': 'release', 'job': 'propose_downstream',"
-        "'metadata': {'dist-git-branch': 'master'}}],"
-        "'downstream_package_name': 'packit'}"
-    )
-    flexmock(
-        GitlabProject,
-        get_file_content=lambda path, ref: packit_yaml,
-        full_repo_name="packit-service/packit",
-    )
-    (
-        flexmock(GitlabProject)
-        .should_receive("can_merge_pr")
-        .with_args("shreyaspapi")
-        .and_return(True)
-    )
-    flexmock(GitlabProject).should_receive("issue_comment").and_return(None)
-    flexmock(GitlabProject).should_receive("issue_close").and_return(None)
-    gr = GitlabRelease(
-        tag_name="0.5.1",
-        url="packit-service/packit",
-        created_at="",
-        tarball_url="https://foo/bar",
-        git_tag=flexmock(GitTag),
-        project=flexmock(GitlabProject),
+        project=flexmock(project_class),
         raw_release=flexmock(),
     )
-    flexmock(GitlabProject).should_receive("get_releases").and_return([gr])
+    flexmock(project_class).should_receive("get_releases").and_return([gr])
     config = ServiceConfig()
     config.command_handler_work_dir = SANDCASTLE_WORK_DIR
     flexmock(ServiceConfig).should_receive("get_service_config").and_return(config)
     flexmock(LocalProject, refresh_the_arguments=lambda: None)
+    flexmock(DistGit).should_receive("local_project").and_return(
+        flexmock(git_project=flexmock(default_branch="main"))
+    )
     flexmock(Allowlist, check_and_report=True)
 
+    yield project_class, issue_comment_propose_downstream_event(forge)
 
-def test_issue_comment_propose_downstream_gitlab_handler(
-    mock_issue_comment_functionality_gitlab,
-    issue_comment_propose_downstream_gitlab_event,
+
+@pytest.mark.parametrize(
+    "mock_comment,event_type",
+    [
+        (
+            (GithubProject, GithubRelease, "github", "phracek"),
+            IssueCommentEvent,
+        ),
+        (
+            (GitlabProject, GitlabRelease, "gitlab", "shreyaspapi"),
+            IssueCommentGitlabEvent,
+        ),
+    ],
+    indirect=[
+        "mock_comment",
+    ],
+)
+def test_issue_comment_propose_downstream_handler(
+    mock_comment,
+    event_type,
 ):
+    project_class, comment_event = mock_comment
+
     flexmock(PackitAPI).should_receive("sync_release").and_return(
         PullRequest(
             title="foo",
@@ -199,21 +133,18 @@ def test_issue_comment_propose_downstream_gitlab_handler(
         )
     )
     flexmock(
-        GitlabProject,
+        project_class,
         get_files=lambda ref, filter_regex: [],
-        get_web_url=lambda: "https://gitlab.com/the-namespace/the-repo",
         is_private=lambda: False,
     )
 
-    flexmock(IssueCommentGitlabEvent, db_trigger=IssueModel(id=123))
+    flexmock(event_type, db_trigger=IssueModel(id=123))
     flexmock(IssueModel).should_receive("get_by_id").with_args(123).and_return(
         flexmock(issue_id=12345)
     )
     flexmock(Signature).should_receive("apply_async").once()
 
-    processing_results = SteveJobs().process_message(
-        issue_comment_propose_downstream_gitlab_event
-    )
+    processing_results = SteveJobs().process_message(comment_event)
     event_dict, job, job_config, package_config = get_parameters_from_results(
         processing_results
     )
