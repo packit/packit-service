79851faaf83c6a873b52dc0d6df9921d73241d2e
Hunor Csomortáni <csomh@redhat.com>
2022-02-25 15:22:21 +0100

    Use 'mod_wsgi-express' instead of 'httpd'
    This does a few things:
    
    1. Removes all httpd configuration. The web-server is configured by
       mod_wsgi-express, and we use mod_wsgi-express command line option to
       tweak things.
    2. Removes some unnecessary dependencies.
    3. Adds exiting with an error if the migration before starting the
       web-server fails for any reason. This wasn't happening before this,
       the web-server just started even if the migration failed.
    4. Removes running 'setup_env_in_openshift' before the web-server. This
       doesn't seem to do anything at this point.
    
    '--process' is set to 2, b/c this seems to be producing the same memory
    footprint as the previous configuration (which it doesn't seem to be
    possible to replicate with the command line options, but maybe it's not
    necessary).
    
    '--locale' is configured in order to avoid warnings at startup regarding
    not being able to set the locale to en_US.UTF-8.
    
    Signed-off-by: Hunor Csomortáni <csomh@redhat.com>

--- files/run_httpd.sh
+++ files/run_httpd.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/bash
 
-set -xe
+set -eux
 
-source /usr/bin/setup_env_in_openshift.sh
-
-pushd /src
 # if all containers started at the same time, pg is definitely not ready to serve
 # so let's try this for a few times
+ATTEMPTS=7
 n=0
-until [ $n -ge 7 ]
-do
+while [[ $n -lt $ATTEMPTS ]]; do
   alembic-3 upgrade head && break
   n=$[$n+1]
   sleep 2
 done
-popd  # pushd /src
+
+# If the number of attempts was exhausted: the migration failed.
+# Exit with an error.
+if [[ $n -eq $ATTEMPTS ]]; then
+    echo "Migration failed after $ATTEMPTS attempts. Exiting."
+    exit 1
+fi
 
 export PACKIT_SERVICE_CONFIG="${HOME}/.config/packit-service.yaml"
 
-httpd -DFOREGROUND
+exec mod_wsgi-express-3 start-server \
+    --https-port 8443 \
+    --access-log \
+    --log-to-terminal \
+    --ssl-certificate-file /secrets/fullchain.pem \
+    --ssl-certificate-key-file /secrets/privkey.pem \
+    --server-name ${DEPLOYMENT}.packit.dev \
+    --processes 2 \
+    --locale "C.UTF-8" \
+    /usr/share/packit/packit.wsgi
