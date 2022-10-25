fbc1a53b63f51234a1d7b2fefc9ff358a3b5d2cd
Maja Massarini <mmassari@redhat.com>
2022-10-25 09:09:45 +0200

    Improve notification repo Checker name
    Co-authored-by: František Nečas <fnecas@redhat.com>

--- packit_service/worker/checker/forges.py
+++ packit_service/worker/checker/forges.py
@@ -9,7 +9,7 @@ from packit_service.worker.mixin import GetIssueMixin
 logger = logging.getLogger(__name__)
 
 
-class PermissionOnForge(Checker, GetIssueMixin):
+class IsIssueInNotificationRepoChecker(Checker, GetIssueMixin):
     def pre_check(self) -> bool:
         """Checks whether the Packit verification command is placed in
         packit/notifications repository in the issue our service created.
