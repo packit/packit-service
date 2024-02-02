4d47ebd65e5a7e7904ee7e207b4481d2e4db2af1
Matej Focko <mfocko@redhat.com>
2024-02-02 11:01:39 +0100

    chore(reporting): factor out news
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- packit_service/worker/reporting/__init__.py
+++ packit_service/worker/reporting/__init__.py
@@ -4,8 +4,9 @@
 import logging
 from datetime import datetime, timezone
 from enum import Enum, auto
-from random import choice
 from typing import Optional, Union, Dict, Callable
+
+from .news import get_random_news_sentence
 
 from ogr.abstract import CommitStatus, GitProject
 from ogr.exceptions import GithubAPIException, GitlabAPIException
@@ -26,35 +27,6 @@ from packit_service.constants import (
 )
 
 logger = logging.getLogger(__name__)
-
-
-NEWS_FOOTER = [
-    "Do you maintain a Fedora package and don't have access to the upstream repository? "
-    "Packit can help. "
-    "Take a look [here](https://packit.dev/posts/pull-from-upstream/) to know more.",
-    "Do you maintain a Fedora package and you think it's boring? Packit can help. "
-    "Take a look [here](https://packit.dev/posts/downstream-automation/) to know more.",
-    "Want to use a build from a different project when testing? "
-    "Take a look [here](https://packit.dev/posts/testing-farm-triggering/) to know more.",
-    "Curious how Packit handles the Release field during propose-downstream? "
-    "Take a look [here](https://packit.dev/posts/release-field-handling/) to know more.",
-    "Did you know Packit is on Mastodon? Or, more specifically, on Fosstodon? "
-    "Follow [@packit@fosstodon.org](https://fosstodon.org/@packit) "
-    "and be one of the first to know about all the news!",
-    "Interested in the Packit team plans and priorities? "
-    "Check [our epic board](https://github.com/orgs/packit/projects/7/views/29).",
-    "Do you use `propose_downstream`? We would be happy if you could help"
-    "us with verifying it in staging. "
-    "See [the details](https://packit.dev/posts/verify-sync-release-volunteers)",
-]
-
-
-def get_random_news_sentence() -> str:
-    """
-    A random sentence to show our users as a footer when adding a status.
-    (Will be visible at the very bottom of the markdown field.
-    """
-    return choice(NEWS_FOOTER)
 
 
 class BaseCommitStatus(Enum):
