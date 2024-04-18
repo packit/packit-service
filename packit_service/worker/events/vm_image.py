993b8c98d6da0da69f8519979209caea3988b4f5
Laura Barcziova <lbarczio@redhat.com>
2024-04-18 10:34:35 +0200

    Fix VMImageBuildResultEvent.get_db_project_event()

--- packit_service/worker/events/vm_image.py
+++ packit_service/worker/events/vm_image.py
@@ -44,5 +44,5 @@ class VMImageBuildResultEvent(AbstractResultEvent):
     def get_db_project_event(self) -> Optional[ProjectEventModel]:
         model = VMImageBuildTargetModel.get_by_build_id(self.build_id)
         for run in model.runs:
-            return run.get_project_event_model()
+            return run.project_event
         return None
