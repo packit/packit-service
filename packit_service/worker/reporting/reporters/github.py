a2548d599a76ddbe7b917910fa17f5a88148e705
Laura Barcziova <lbarczio@redhat.com>
2024-07-29 12:26:37 +0200

    Add newline after markdown content for GitHub reporting

--- packit_service/worker/reporting/reporters/github.py
+++ packit_service/worker/reporting/reporters/github.py
@@ -107,6 +107,7 @@ class StatusReporterGithubChecks(StatusReporterGithubStatuses):
         summary = (
             self._create_table(url, links_to_external_services)
             + markdown_content
+            + "\n"
             + f"---\n*{News.get_sentence()}*"
         )
 
