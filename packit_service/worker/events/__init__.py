afebe192daea7c6cb6cadd810564b1925dbdc497
Matej Focko <mfocko@redhat.com>
2024-11-26 15:43:21 +0100

    fix(events): rename TF results
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- packit_service/worker/events/__init__.py
+++ packit_service/worker/events/__init__.py
@@ -64,7 +64,7 @@ from packit_service.worker.events.pagure import (
     PullRequestPagureEvent,
     PushPagureEvent,
 )
-from packit_service.worker.events.testing_farm import TestingFarmResultsEvent
+from packit_service.worker.events.testing_farm import Result as TestingFarmResultsEvent
 from packit_service.worker.events.vm_image import Result as VMImageBuildResultEvent
 
 __all__ = [
