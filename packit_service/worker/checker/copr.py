fd28dfce40ee7d36f22f8e55cb7e534f7020a6f5
Cristian Le <git@lecris.dev>
2025-04-16 15:33:38 +0200

    [WIP] Implement AreFilesChanged
    Signed-off-by: Cristian Le <git@lecris.dev>

--- packit_service/worker/checker/copr.py
+++ packit_service/worker/checker/copr.py
@@ -2,11 +2,14 @@
 # SPDX-License-Identifier: MIT
 
 import logging
+from pathlib import Path
+
+from packit.config import JobConfigTriggerType
 
 from packit_service.constants import (
     INTERNAL_TF_BUILDS_AND_TESTS_NOT_ALLOWED,
 )
-from packit_service.events import gitlab
+from packit_service.events import github, gitlab
 from packit_service.worker.checker.abstract import (
     ActorChecker,
     Checker,
@@ -140,12 +143,51 @@ class CanActorRunTestsJob(
         return True
 
 
-class AreFilesChanged(Checker):
+class AreFilesChanged(Checker, GetCoprBuildJobHelperForIdMixin, ConfigFromEventMixin):
     """
     Check if any files under the current package's `paths` field is changed.
     If not, then just skip the current copr build job.
     """
 
+    def get_files_changed(self) -> list[Path]:
+        """
+        Get the list of files changed in the current commit or the current pullrequest
+        """
+        # Get the changes object
+        if self.job_config.trigger == JobConfigTriggerType.pull_request:
+            pr_event = self.data.to_event()
+            if not isinstance(pr_event, (github.pr.Action, gitlab.mr.Action)):
+                # TODO: What about comments?
+                raise NotImplementedError()
+            # TODO: How to handle PRs?
+            raise NotImplementedError()
+        if self.job_config.trigger == JobConfigTriggerType.commit:
+            push_event = self.data.to_event()
+            if not isinstance(push_event, (github.push.Commit, gitlab.push.Commit)):
+                raise NotImplementedError()
+            files = set()
+            for commit in push_event.commits:
+                files |= set(commit["modified"])
+                files |= set(commit["added"])
+            # TODO: Check what the path is relative to
+            return [Path(file) for file in files]
+        raise NotImplementedError(f"Trigger not supported: {self.job_config.trigger}")
+
     def pre_check(self) -> bool:
-        # TODO: Implement the logic to check if relevant files are changed
-        return True
+        if self.job_config.trigger == JobConfigTriggerType.release:
+            # For releases we don't do any checks
+            return True
+        # FIXME: This is probably unnecessary
+        package_config = self.package_config.get_package_config_for(self.job_config)
+        # The paths that we need to check for files changed
+        paths = package_config["paths"]
+        # Early check if the git root was included, in which case we don't need to
+        # check the files changed
+        # TODO: refine this check when gitignore-like patterns are supported
+        if "./" in paths:
+            return True
+        for changed_file in self.get_files_changed():
+            # Check if any of the files changed are under the paths that are being tracked
+            if any(changed_file.is_relative_to(p) for p in paths):
+                return True
+        return False
