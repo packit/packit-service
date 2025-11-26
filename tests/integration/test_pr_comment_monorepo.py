6df3d8ce8f8a6971c59128a375b6dc995b6603ef
Alzbeta Kucerova <akucerov@redhat.com>
2025-11-26 10:31:04 +0100

    Remove unused variable to satisfy ruff

--- tests/integration/test_pr_comment_monorepo.py
+++ tests/integration/test_pr_comment_monorepo.py
@@ -150,7 +150,7 @@ def test_pr_comment_monorepo_build_build_and_test_handler(
     test_job = [item for item in processing_results if item["details"]["job"] == "tests"]
     assert test_job
 
-    event_dict, job, job_config, package_config = get_parameters_from_results(test_job)
+    event_dict, _, job_config, package_config = get_parameters_from_results(test_job)
     assert json.dumps(event_dict)
     results = run_testing_farm_handler(
         package_config=package_config,
