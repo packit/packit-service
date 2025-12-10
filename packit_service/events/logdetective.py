dca49e8f163d1905152189a7462791d141bb798f
Jiri Podivin <jpodivin@redhat.com>
2025-12-10 12:29:43 +0100

    Adding parsing for log_detective_analysis_start
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- packit_service/events/logdetective.py
+++ packit_service/events/logdetective.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 import logging
+from datetime import datetime
 from typing import Optional
 
 from packit_service.models import (
@@ -28,6 +29,7 @@ class Result(AbstractResult):
         status: LogDetectiveResult,
         build_system: LogDetectiveBuildSystem,
         identifier: str,
+        log_detective_analysis_start: datetime,
     ):
         super().__init__()
         self.target_build = target_build
@@ -35,6 +37,7 @@ class Result(AbstractResult):
         self.status = status
         self.build_system = build_system
         self.identifier = identifier
+        self.log_detective_analysis_start = log_detective_analysis_start
 
     def get_dict(self, default_dict: Optional[dict] = None) -> dict:
         """Return Log Detective result as a dictionary,
@@ -42,5 +45,5 @@ class Result(AbstractResult):
         result = super().get_dict()
         result["status"] = self.status.value
         result["build_system"] = self.build_system.value
-
+        result["log_detective_analysis_start"] = str(self.log_detective_analysis_start)
         return result
