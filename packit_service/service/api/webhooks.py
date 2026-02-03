packit/avant

Implement Forgejo event handling in Packit Service

packit_service/service/api/webhooks.py
@@ -45,6 +45,15 @@ ping_payload_gitlab = ns.model(
     },
 )
 
+ping_payload_forgejo = ns.model(
+    "Forgejo webhook ping",
+    {
+        "zen": fields.String(required=False),
+        "hook_id": fields.String(required=False),
+        "hook": fields.String(required=False),
+    },
+)
+
 github_webhook_calls = Counter(
     "github_webhook_calls",
     "Number of times the GitHub webhook is called",
@@ -343,4 +352,133 @@ class GitlabWebhook(Resource):
         _interested = event_type in interesting_events
 
         logger.debug(f"{event_type} {' (not interested)' if not _interested else ''}")
+        return _interested
+
+
+forgejo_webhook_calls = Counter(
+    "forgejo_webhook_calls",
+    "Number of times the Forgejo webhook is called",
+    ["result", "process_id"],
+)
+
+
+@ns.route("/forgejo")
+class ForgejoWebhook(Resource):
+    @ns.response(HTTPStatus.OK.value, "Webhook accepted, returning reply")
+    @ns.response(
+        HTTPStatus.ACCEPTED.value,
+        "Webhook accepted, request is being processed",
+    )
+    @ns.response(HTTPStatus.BAD_REQUEST.value, "Bad request data")
+    @ns.response(HTTPStatus.UNAUTHORIZED.value, "X-Forgejo-Signature validation failed")
+    @ns.expect(ping_payload)
+    def post(self):
+        msg = request.json
+
+        if not msg:
+            logger.debug("/webhooks/forgejo: no JSON data received.")
+            forgejo_webhook_calls.labels(result="no_data", process_id=os.getpid()).inc()
+
+            return "No JSON data.", HTTPStatus.BAD_REQUEST
+
+        if all([msg.get("zen"), msg.get("hook_id"), msg.get("hook")]):
+            logger.debug(f"/webhooks/forgejo received ping event: {msg['hook']}")
+            forgejo_webhook_calls.labels(result="pong", process_id=os.getpid()).inc()
+            return "Pong!", HTTPStatus.OK
+        # TODO
+        # try:
+        #     self.validate_token()
+        # except ValidationFailed as exc:
+        #     logger.info(f"/webhooks/forgejo {exc}")
+        #     forgejo_webhook_calls.labels(
+        #         result="invalid_signature",
+        #         process_id=os.getpid(),
+        #     ).inc()
+        #     return str(exc), HTTPStatus.UNAUTHORIZED
+        #
+        if not self.interested(msg):
+            forgejo_webhook_calls.labels(
+                result="not_interested",
+                process_id=os.getpid(),
+            ).inc()
+            return "Thanks but we don't care about this event", HTTPStatus.ACCEPTED
+
+        celery_app.send_task(
+            name=getenv("CELERY_MAIN_TASK_NAME") or CELERY_DEFAULT_MAIN_TASK_NAME,
+            kwargs={
+                "event": msg,
+                "source": "forgejo",
+                "event_type": request.headers.get("X-Forgejo-Event"),
+            },
+        )
+        forgejo_webhook_calls.labels(result="accepted", process_id=os.getpid()).inc()
+
+        return "Webhook accepted. We thank you, Forgejo.", HTTPStatus.ACCEPTED
+
+    def validate_token(self):
+        """
+        Validate the Forgejo webhook signature.
+        The signature is a direct SHA256 HMAC hex digest of the raw request body
+        using the webhook secret as the key, in a similar fashion to Github.
+
+        """
+        if "X-Forgejo-Signature" not in request.headers:
+            logger.debug("Ain't validating signatures.")
+            return
+
+        if not (webhook_secret := getenv("WEBHOOK_SECRET")):
+            msg = "'webhook_secret' not specified in the config."
+            logger.error(msg)
+            raise ValidationFailed(msg)
+
+        # Get raw payload
+
+        payload = request.get_data()
+        if not payload:
+            msg = "No payload received."
+            logger.error(msg)
+            raise ValidationFailed(msg)
+
+        data_hmac = hmac.new(webhook_secret.encode(), msg=payload, digestmod=sha256)
+        payload_signature = data_hmac.hexdigest()
+        header_sig = request.headers["X-Forgejo-Signature"]
+
+        if header_sig != payload_signature:
+            msg = "Payload signature validation failed."
+
+            logger.warning(msg)
+            logger.debug(
+                f"X-Forgejo-Signature: {header_sig!r} != computed: {payload_signature}",
+            )
+            raise ValidationFailed(msg)
+
+    @staticmethod
+    def interested(msg):
+        """
+
+        Check whether we want to process this event.
+
+
+        Args:
+            msg: The webhook payload as a dictionary
+
+        Returns:
+            bool: False if we are not interested in this kind of event
+        """
+        event = request.headers.get("X-Forgejo-Event")
+        uuid = request.headers.get("X-Forgejo-Delivery")
+        action = msg.get("action") if msg else None
+        deleted = msg.get("deleted") if msg else None
+
+        interests = {
+            "push": not deleted,
+            "release": action == "published",
+            "issues": action in {"opened", "edited", "closed", "reopened"},
+            "issue_comment": action in {"created", "edited"},
+            "pull_request": action in {"opened", "edited", "closed", "reopened", "synchronize"},
+        }
+
+        _interested = interests.get(event or "", False)
+
+        logger.debug(f"{event} {uuid}{'' if _interested else ' (not interested)'}")
         return _interested

packit/packit-service

packit_service/service/api/webhooks.py
@@ -45,6 +45,15 @@ ping_payload_gitlab = ns.model(
     },
 )
 
+ping_payload_forgejo = ns.model(
+    "Forgejo webhook ping",
+    {
+        "zen": fields.String(required=False),
+        "hook_id": fields.String(required=False),
+        "hook": fields.String(required=False),
+    },
+)
+
 github_webhook_calls = Counter(
     "github_webhook_calls",
     "Number of times the GitHub webhook is called",
@@ -343,4 +352,133 @@ class GitlabWebhook(Resource):
         _interested = event_type in interesting_events
 
         logger.debug(f"{event_type} {' (not interested)' if not _interested else ''}")
+        return _interested
+
+
+forgejo_webhook_calls = Counter(
+    "forgejo_webhook_calls",
+    "Number of times the Forgejo webhook is called",
+    ["result", "process_id"],
+)
+
+
+@ns.route("/forgejo")
+class ForgejoWebhook(Resource):
+    @ns.response(HTTPStatus.OK.value, "Webhook accepted, returning reply")
+    @ns.response(
+        HTTPStatus.ACCEPTED.value,
+        "Webhook accepted, request is being processed",
+    )
+    @ns.response(HTTPStatus.BAD_REQUEST.value, "Bad request data")
+    @ns.response(HTTPStatus.UNAUTHORIZED.value, "X-Forgejo-Signature validation failed")
+    @ns.expect(ping_payload_forgejo)
+    def post(self):
+        msg = request.json
+
+        if not msg:
+            logger.debug("/webhooks/forgejo: no JSON data received.")
+            forgejo_webhook_calls.labels(result="no_data", process_id=os.getpid()).inc()
+
+            return "No JSON data.", HTTPStatus.BAD_REQUEST
+
+        if all([msg.get("zen"), msg.get("hook_id"), msg.get("hook")]):
+            logger.debug(f"/webhooks/forgejo received ping event: {msg['hook']}")
+            forgejo_webhook_calls.labels(result="pong", process_id=os.getpid()).inc()
+            return "Pong!", HTTPStatus.OK
+        # TODO
+        # try:
+        #     self.validate_token()
+        # except ValidationFailed as exc:
+        #     logger.info(f"/webhooks/forgejo {exc}")
+        #     forgejo_webhook_calls.labels(
+        #         result="invalid_signature",
+        #         process_id=os.getpid(),
+        #     ).inc()
+        #     return str(exc), HTTPStatus.UNAUTHORIZED
+        #
+        if not self.interested(msg):
+            forgejo_webhook_calls.labels(
+                result="not_interested",
+                process_id=os.getpid(),
+            ).inc()
+            return "Thanks but we don't care about this event", HTTPStatus.ACCEPTED
+
+        celery_app.send_task(
+            name=getenv("CELERY_MAIN_TASK_NAME") or CELERY_DEFAULT_MAIN_TASK_NAME,
+            kwargs={
+                "event": msg,
+                "source": "forgejo",
+                "event_type": request.headers.get("X-Forgejo-Event"),
+            },
+        )
+        forgejo_webhook_calls.labels(result="accepted", process_id=os.getpid()).inc()
+
+        return "Webhook accepted. We thank you, Forgejo.", HTTPStatus.ACCEPTED
+
+    def validate_token(self):
+        """
+        Validate the Forgejo webhook signature.
+        The signature is a direct SHA256 HMAC hex digest of the raw request body
+        using the webhook secret as the key, in a similar fashion to Github.
+
+        """
+        if "X-Forgejo-Signature" not in request.headers:
+            logger.debug("Ain't validating signatures.")
+            return
+
+        if not (webhook_secret := getenv("WEBHOOK_SECRET")):
+            msg = "'webhook_secret' not specified in the config."
+            logger.error(msg)
+            raise ValidationFailed(msg)
+
+        # Get raw payload
+
+        payload = request.get_data()
+        if not payload:
+            msg = "No payload received."
+            logger.error(msg)
+            raise ValidationFailed(msg)
+
+        data_hmac = hmac.new(webhook_secret.encode(), msg=payload, digestmod=sha256)
+        payload_signature = data_hmac.hexdigest()
+        header_sig = request.headers["X-Forgejo-Signature"]
+
+        if header_sig != payload_signature:
+            msg = "Payload signature validation failed."
+
+            logger.warning(msg)
+            logger.debug(
+                f"X-Forgejo-Signature: {header_sig!r} != computed: {payload_signature}",
+            )
+            raise ValidationFailed(msg)
+
+    @staticmethod
+    def interested(msg):
+        """
+
+        Check whether we want to process this event.
+
+
+        Args:
+            msg: The webhook payload as a dictionary
+
+        Returns:
+            bool: False if we are not interested in this kind of event
+        """
+        event = request.headers.get("X-Forgejo-Event")
+        uuid = request.headers.get("X-Forgejo-Delivery")
+        action = msg.get("action") if msg else None
+        deleted = msg.get("deleted") if msg else None
+
+        interests = {
+            "push": not deleted,
+            "release": action == "published",
+            "issues": action in {"opened", "edited", "closed", "reopened"},
+            "issue_comment": action in {"created", "edited"},
+            "pull_request": action in {"opened", "edited", "closed", "reopened", "synchronize"},
+        }
+
+        _interested = interests.get(event or "", False)
+
+        logger.debug(f"{event} {uuid}{'' if _interested else ' (not interested)'}")
         return _interested